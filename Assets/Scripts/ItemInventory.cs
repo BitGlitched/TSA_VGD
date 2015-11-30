78de182556eb6bd726a24ec98781b4924a516f3c
Andrew Legg <flamewarrior558@yahoo.com>
2015-11-30 00:15:57 -0500

    More changes to item pickups and inventory, a few bugs left and then its done

--- Assets/Scripts/ItemInventory.cs
+++ Assets/Scripts/ItemInventory.cs
@@ -3,21 +3,43 @@ using System.Collections;
 
 public class ItemInventory : MonoBehaviour
 {
-	public bool hasDoubleJumpUpgrade;
-
-	// Update is called once per frame
-	void Update ()
-	{
-		
-	}
+	public bool hasDoubleJump;
+	public bool hasWallJump;
+	public bool hasJetpack;
 
 	void AddItem (string itemName)
 	{
+		if (itemName == "Double Jump")
+		{
+			hasDoubleJump = true;
+		}
 
+		if (itemName == "Wall Jump")
+		{
+			hasWallJump = true;
+		}
+
+		if (itemName == "Jetpack")
+		{
+			hasJetpack = true;
+		}
 	}
 
 	void RemoveItem (string itemName)
 	{
-
+		if (itemName == "Double Jump")
+		{
+			hasDoubleJump = false;
+		}
+		
+		if (itemName == "Wall Jump")
+		{
+			hasWallJump = false;
+		}
+		
+		if (itemName == "Jetpack")
+		{
+			hasJetpack = false;
+		}
 	}
 }
--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -3,18 +3,26 @@ using System.Collections;
 
 public class ItemPickup : MonoBehaviour
 {
+	public LayerMask playerLayer;
 	public string itemName;
 
+	private GameObject player;
 	private Collider2D pickupCollider;
 
 	void Awake()
 	{
+		player = GameObject.FindGameObjectWithTag("Player");
 		pickupCollider = this.gameObject.GetComponent<Collider2D>();
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
+		if (pickupCollider.IsTouching(player.GetComponent<Collider2D>()));
+		{
+			player.SendMessage("AddItem", itemName, SendMessageOptions.RequireReceiver);
 
+			Destroy(this.gameObject);
+		}
 	}
 }
